163c41abf7926278f6e0b709797a6a7c594126cc
Micah Levine <micah.levine@outlook.com>
2019-08-15 21:57:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,7 @@
-def reverse_each_word(string)
+def reverse_each_word
   original_array = string.split(" ")
   return_array = []
   original_array.each do|string|
-    return_array << string.reverse
-  end
-  return_array.join(" ")
-end
-
-def reverse_each_word(string)
-  original_array = string.split(" ")
-  return_array = []
-  original_array.collect do|string|
     return_array << string.reverse
   end
   return_array.join(" ")
